9da7ad0629def087697c7b8d43ba5965c8d2e72b
umadevi-dharmaraj <59370692+umadevi-dharmaraj@users.noreply.github.com>
2022-10-14 22:31:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Protected from './components/Protected';
+import { AuthContextProvider } from './context/AuthContext';
+import Account from './pages/Account';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import User from './pages/User';
+
+function App() {
+  return (
+    <div>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route
+            path='/account'
+            element={
+              <Protected>
+                <Account />
+              </Protected>
+            }
+          />
+          <Route path='/user' element={<User />} />
+          <Route path='/home' element={<Home />} />
+
+        </Routes>
+      </AuthContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Account.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { UserAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+ var userDetails =[];
+ 
+
+ const Account = () => {
+  const navigate = useNavigate();
+
+  const navigateToUser = () => {
+    navigate('/user');
+  };
+  const { logOut, user } =  UserAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const providerdata = JSON.stringify(user)
+  const displayname = user.displayName
+ 
+  return (
+
+    <div className='w-[300px] m-auto text-center'>
+      <h1 className='text-center text-3xl font-bold pt-20'>Account</h1>
+      <div>
+        <p style={{fontSize:25}}>Welcome, {displayname}</p>
+        
+        
+        
+      </div>
+      <button onClick={handleSignOut} style={{fontSize:25,backgroundColor:"greenyellow"}} className='border py-5 px-10 mt-9 ml-16'>
+        Logout
+      </button>
+      <button onClick={navigateToUser} style={{fontSize:25,backgroundColor:"greenyellow"}} className='border py-5 px-10 mt-9 ml-16'>Users</button>
+    </div>
+  
+  );
+};
+
+export default Account;
